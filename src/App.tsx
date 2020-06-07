58970650b12ae3aaa811e100cb5eb808bf9b1c4b
Andreza Telles Cascais <shareingprojects@gmail.com>
2020-06-07 18:57:48 -0300

    Third commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './Header';
+import Routes from './routes';
 
 function App() {
-
-  const [counter, setCounter] = useState(0);
-
-  function handleButtonClick(){
-     setCounter(counter + 1);      
-  }
-
- return ( 
-       <div>
-          <Header title="Home" />
-          <Header title="About us" />
-          <Header title="Contact" />
-
-          <h1>{counter}</h1>
-          
-         <button type="button" onClick={handleButtonClick}>increased</button>        
-      </div>
-  );
+    return (        
+         <Routes />    
+        
+    );
  }
 export default App;
  
